6a77d0ce055a9d53aa5ea3ef4caa989708b566b9
Pablo Santos <pablomoreirasantos.hp@gmail.com>
2026-03-25 16:17:28 -0300

    add method to find active client by ID in ClientRepository

--- src/main/java/com/autopro/autoproapi/clients/repository/ClientRepository.java
+++ src/main/java/com/autopro/autoproapi/clients/repository/ClientRepository.java
@@ -4,9 +4,11 @@ import com.autopro.autoproapi.clients.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClientRepository extends JpaRepository<Client, UUID> {
     boolean existsByCpf(String cpf);
     List<Client> findByIsActiveTrue();
+    Optional<Client> findByIdAndIsActiveTrue(UUID id);
 }
